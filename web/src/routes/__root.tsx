6d87214c67a9a796adb7d1fcfc686aca32407a5c
igorb <igorbabin2002@gmail.com>
2025-11-14 00:39:38 +0300

    Added opensans font

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -8,6 +8,7 @@ import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
+import '@fontsource/open-sans/500.css';
 import '@/styles.css';
 
 import type { QueryClient } from '@tanstack/react-query'
